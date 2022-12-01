eb665ba949e4b091c1896314d3a7ac1b6626b5e8
Umesh Agarwal <54064940+Umesh5245@users.noreply.github.com>
2022-12-01 18:01:16 +0530

    Time: 17 ms (39.80%), Space: 5.8 MB (78.67%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,16 +3,8 @@ class Solution
     public:
         int fib(int n)
         {
-            if (n < 2) return n;
-
-            int prev1 = 1, prev2 = 0;
-            for (int i = 2; i <= n; i++)
-            {
-                int temp = prev1;
-                prev1 += prev2;
-                prev2 = temp;
-            }
-
-            return prev1;
+            if (n <= 1)
+                return n;
+            return fib(n - 1) + fib(n - 2);
         }
 };
